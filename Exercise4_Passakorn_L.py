f951c0569830162165f6a0a7ab147709c398faca
ppunnpsk <102904282+ppunnpsk@users.noreply.github.com>
2023-02-04 15:38:18 +0700

    Create Exercise4_Passakorn_L.py

---
+++ Exercise4_Passakorn_L.py
@@ -0,0 +1,9 @@
+print("--- Your Score ---")
+fe = 6.78
+gb = 8.0
+ics = 5.87
+cp = 9.9
+print("Foundation English : ",fe)
+print("General Business : ",gb)
+print("Introduction to Computer Systems : ",ics)
+print("Computer Programming : ",cp)
